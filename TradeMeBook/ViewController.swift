f87f130538004d28c740ea63c78be347c45df7aa
Dronya_b <Dronya@dronya-2.local>
2018-05-19 16:23:05 +0700

    Initial Commit

---
+++ TradeMeBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TradeMeBook
+//
+//  Created by Dronya_b on 5/19/18.
+//  Copyright © 2018 Vaifathuy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
